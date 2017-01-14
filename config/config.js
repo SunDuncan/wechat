b4469d100f77c67f033444b899e2ff9a9c35786b
Duncan <1466863073@qq.com>
2017-01-14 16:32:02 +0800

    Update:增加了config的简单构成

---
+++ config/config.js
@@ -0,0 +1,17 @@
+/**
+ * @Author: Duncan
+ * @Date: 2017-01-14
+ * @Last Modified by:
+ * @Last Modified time:
+ * @function: 微信相关，需要默认的确定的变量，放在这以便好调用
+ */
+ 
+ var config = {
+	 app_name: 'wechatDevelop',
+	 app_description: '用nodejs来开发微信的程序',
+	 weChat: {
+		 baseUrl: 'https://api.weixin.qq.com/cgi-bin/'
+	 }
+ };
+ 
+ module.exports = config;
